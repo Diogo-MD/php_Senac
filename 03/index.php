f0aecb39aae32555cbedbbf1ec7e5836aa3bf53d
Diogo-MD <diogomdossantos@gmail.com>
2024-03-22 19:51:02 -0300

    Aula 03 - Números e Operadores

---
+++ 03/index.php
@@ -0,0 +1,27 @@
+<?php
+//// Números e Operadores
+
+// Operações Básicas
+$num1 = 10;
+$num2 = 5;
+echo "Valor A: " . $num1 . " e valor B: " . $num2;
+echo "<br><br>";
+
+// Soma
+$soma = $num1 + $num2;
+echo "Soma: " . $soma . "<br>";
+
+// Subtração
+$subtracao = $num1 - $num2;
+echo "Subtração: " . $subtracao . "<br>";
+
+// Multiplicação
+$multiplicacao = $num1 * $num2;
+echo "Multiplicação: " . $multiplicacao . "<br>";
+
+// Divisão
+$divisao = $num1 / $num2;
+echo "Divisão: " . $divisao . "<br>";
+
+
+?>
