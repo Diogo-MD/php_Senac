ea222556314c2e06984bd35c01ec68b3b5d5cbd4
Diogo-MD <diogomdossantos@gmail.com>
2024-03-22 20:24:25 -0300

    Aula 03 - Operadores de Comparação

--- 03/index.php
+++ 03/index.php
@@ -4,24 +4,62 @@
 // Operações Básicas
 $num1 = 10;
 $num2 = 5;
-echo "Valor A: " . $num1 . " e valor B: " . $num2;
-echo "<br><br>";
+echo "Valor A: $num1 e Valor B: $num2<br><br>";
 
 // Soma
 $soma = $num1 + $num2;
-echo "Soma: " . $soma . "<br>";
+echo "Soma: $soma <br>";
 
 // Subtração
 $subtracao = $num1 - $num2;
-echo "Subtração: " . $subtracao . "<br>";
+echo "Subtração: $subtracao <br>";
 
 // Multiplicação
 $multiplicacao = $num1 * $num2;
-echo "Multiplicação: " . $multiplicacao . "<br>";
+echo "Multiplicação: $multiplicacao <br>";
 
 // Divisão
 $divisao = $num1 / $num2;
-echo "Divisão: " . $divisao . "<br>";
+echo "Divisão: $divisao <br>";
 
+// Módulo (Resto da divisão)
+$modulo = $num1 % $num2;
+echo "Módulo: $modulo <br>";
+
+
+
+// Operações de Incremento e Decremento
+$num = 5;
+echo "Valor base para incremento e decremento = $num <br>";
+
+// Incremento
+$num++;
+echo "Incremento: $num <br>";
+
+// Decremento
+$num--;
+echo "Decremento: $num <br>";
+
+
+
+// Operadores de Comparação
+$num1 = 10;
+$num2 = 5;
+
+// Igualdade
+echo "Igual a: ";
+echo $num1 == $num2 ? "Verdadeiro <br>" : "Falso <br>";
+
+// Diferente
+echo "Diferente de: ";
+echo $num1 != $num2 ? "Verdadeiro <br>" : "Falso <br>";
+
+// Maior que
+echo "Maior que: ";
+echo $num1 > $num2 ? "Verdadeiro <br>" : "Falso <br>";
+
+// Menor que 
+echo "Menor que: ";
+echo $num1 < $num2 ? "Verdadeiro <br>" : "Falso <br>    ";
 
 ?>
