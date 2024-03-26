d300f91495d23f3bef17b7625b36dca62219f28a
Diogo-MD <diogomdossantos@gmail.com>
2024-03-26 20:47:19 -0300

    Aula 05 - Array's

--- 05/index.php
+++ 05/index.php
@@ -4,7 +4,7 @@
 // Definição de Array's 
 $frutas = array("Maçã", "Banana", "Laranja", "Morango");
 $numeros = [1, 2, 3, 4, 5];
-$alunos = array ("João" => 18, "Maria" => 2, "Pedro" => 19);
+$alunos = array("João" => 18, "Maria" => 2, "Pedro" => 19);
 
 // Acessando elementos do Array
 echo "A segunda fruta é: " . $frutas[1] . "<br>";
@@ -18,11 +18,33 @@ $alunos["Maria"] = 21;
 
 // Adicionando Elementos ao Array
 $frutas[] = "Abacaxi";
-$numeros = 6;
+$numeros[] = 6;
 $alunos["Ana"] = 22;
 
-// Percorrento um Array com um foreach
+// Percorrendo um Array com um foreach
+echo "Lista de frutas: ";
+foreach ($frutas as $fruta) {
+    echo " " . $fruta;
+}
+echo "<br>";
 
+// Percorrendo um Array com um foreach
+echo "Lista de números: ";
+foreach ($numeros as $numero) {
+    echo " " . $numero;
+}
+echo "<br>";
 
+// Percorrendo um Array com um foreach
+echo "Lista de alunos: ";
+foreach ($alunos as $nome => $idade) {
+    echo "<br>" . $nome . ", e sua idade: " . $idade;
+}
+echo "<br>";
+
+// Funções úteis para Arrays
+echo "Número de elementos no Array de Frutas: " . count ($frutas) . "<br>";
+echo "Índice da fruta 'Laranja' no Array: " . array_search("Laranja",  $frutas) . "<br>";
+echo "Última fruta do Array: " . end($frutas) . "<br>";
 
 ?>
